08ba9a6a9f5200b6950161048e6b36eb7f6b02ab
koi-freitas <victorfrei@gmail.com>
2021-03-18 14:30:28 -0300

    injecao de dependencia implementada atraves da implementacao e criacao da classe de servicos

--- course-manager/src/app/courses/course-list.component.ts
+++ course-manager/src/app/courses/course-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from './course';
+import { CourseService } from './course.service';
 
 @Component({
     selector: 'app-course-list',
@@ -9,29 +10,12 @@ export class CourseListComponent implements OnInit {
   
     courses: Course[] = [];
 
+    constructor(private courseService: CourseService) {
+
+    }
+
     ngOnInit(): void{
-        this.courses = [
-            {
-                id: 1,
-                name: 'Angular: Forms',
-                imageUrl: '/assets/images/forms.png',
-                price: 99.99,
-                code: 'XPS-8796',
-                duration: 120,
-                rating: 4.5,
-                releaseDate:'March, 16, 2021'
-            },
-            {
-                id: 2,
-                name: 'Angular: HTTP',
-                imageUrl: '/assets/images/http.png',
-                price: 45.99,
-                code: 'LKL-1094',
-                duration: 80,
-                rating: 3.5,
-                releaseDate:'March, 14, 2021'
-            }
-        ]
+        this.courses = this.courseService.retrieveAll();
     }
 
 
