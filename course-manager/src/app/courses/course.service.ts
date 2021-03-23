11ad4834808cc89e21233157cff8a06451a56d20
koi-freitas <victorfrei@gmail.com>
2021-03-23 15:38:43 -0300

    requisicao via http em node.js implementada

--- course-manager/src/app/courses/course.service.ts
+++ course-manager/src/app/courses/course.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 import { Course } from "./course";
 
 @Injectable({
@@ -6,8 +8,12 @@ import { Course } from "./course";
 })
 export class CourseService {
 
-    retrieveAll(): Course[] {
-        return COURSES;
+    private coursesUrl: string ='http://localhost:3100/api/courses';
+
+    constructor(private httpClient: HttpClient) {}
+
+    retrieveAll(): Observable<Course[]> {
+        return this.httpClient.get<Course[]>(this.coursesUrl);
     }
 
     retrieveById(id:number): Course {
